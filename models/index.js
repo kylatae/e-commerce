669870034b2e0bf118bfa86c1d60f9b322770548
Arthur Henningfield <144608814+kylatae@users.noreply.github.com>
2023-11-12 11:09:37 -0600

    updated index model

--- models/index.js
+++ models/index.js
@@ -5,12 +5,40 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  through: {
+    model: Product,
+    unique: false
+  },
+  as: 'product_category'
+});
 
 // Categories have many Products
+Category.belongsToMany(Product, {
+  through: {
+    model: Product,
+    unique: false
+  },
+  as: 'category_product'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tag'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_product'
+});
 
 module.exports = {
   Product,
